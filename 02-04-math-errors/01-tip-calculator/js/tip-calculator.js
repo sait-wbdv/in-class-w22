068541dce53af22f1d42ba9437070700b1801a9f
Tony Grimes <acidtone@tonygrimes.com>
2022-02-06 12:55:29 -0700

    end of W4F

---
+++ 02-04-math-errors/01-tip-calculator/js/tip-calculator.js
@@ -0,0 +1,9 @@
+// Assign initial values
+const billTotal = 40;
+const tipRate = .18;
+
+// Calculate tip
+const tipAmount = billTotal * tipRate;
+
+// Output result with template literals
+console.log(`Tip for $${billTotal} at ${tipRate * 100}%: $${tipAmount.toFixed(2)}.`);
---
+++ 02-04-math-errors/03-debugging/js/calculator.js
@@ -0,0 +1,21 @@
+// Create a variable for the form
+const form = document.querySelector('form');
+
+const handleSubmit = function(event) {
+  // Stop form from submitting and refreshing the page
+  event.preventDefault();
+
+  // Assign operands
+  const num1 = Number(form.first.value);
+  const num2 = Number(form.second.value);
+
+  // Calculate total
+  const total = num1 + num2;
+
+  // Print total to page
+  form.total.value = total;
+  // console.log(event);
+}
+
+form.addEventListener('submit', handleSubmit);
+// console.log(form);
