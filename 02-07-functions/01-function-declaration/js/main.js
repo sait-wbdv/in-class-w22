43a184075420d73205e780bd0cbf5b1934da067a
Tony Grimes <acidtone@tonygrimes.com>
2022-02-07 10:26:04 -0700

    added mid-morning

--- 02-07-functions/01-function-declaration/js/main.js
+++ 02-07-functions/01-function-declaration/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+const greet = function(name, age, mentalAge) {
+  console.log(`Hello, my name is ${name}! I'm ${age} years old, but I'm ${mentalAge} on the inside.`);
+}
+
+const name = prompt('What is your name?');
+let age = prompt('How old are you?');
+const mentalAge = prompt('How old are you on the inside?');
+
+greet(name, age, mentalAge);
