a9a0e69856f0d614b94743a9e12c80921fb0829b
Tony Grimes <acidtone@tonygrimes.com>
2022-02-08 10:23:35 -0700

    tip calculator finished

--- 02-08-events/04-tip-calculator-button-starter/tip-calculator.js
+++ 02-08-events/04-tip-calculator-button-starter/tip-calculator.js
@@ -9,12 +9,21 @@ const calcTip = (subTotal, rate) => {
 }
 
 // 1. Wrap prompts inside an clickHandler() function
-const billTotal = prompt('Enter a bill total.');
-const tipRate = prompt('Enter a tip rate.');
 
-calcTip(billTotal, tipRate);
+const clickHandler = () => {
+  
+  const billTotal = prompt('Enter a bill total.');
+  const tipRate = prompt('Enter a tip rate.');
+  
+  calcTip(billTotal, tipRate);
+
+}
 
 // 2. Instead of manually invoking function, attach it to a button as an event listener
 // 2a. Create a button variable
+const button = document.querySelector('.tip-calc');
+
+// button.innerHTML = "Start Tip Calculator"
 
 // 2b. Attach (clickHandler to button with a 'click' event)
+button.addEventListener('click', clickHandler);
