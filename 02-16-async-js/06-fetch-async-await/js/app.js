436d4c42510e9ac489bea727a0cd2c1ffa7cda80
Tony Grimes <acidtone@tonygrimes.com>
2022-02-16 11:25:06 -0700

    added fetch finished code

--- 02-16-async-js/06-fetch-async-await/js/app.js
+++ 02-16-async-js/06-fetch-async-await/js/app.js
@@ -1,1 +1,14 @@
+
+const fetchData = async function() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/9');
+  const data = await response.json();
+  
+  console.log(data);
+
+  document.querySelector('h1').textContent = data.title;
+  document.querySelector('p').textContent = data.body;
+}
+
+fetchData();
+
 console.log('Hello world!');
